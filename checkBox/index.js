ea38ed35bd606b463c8dea58b79bd01d52186ab6
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-08 14:31:36 +0530

    add

--- checkBox/index.js
+++ checkBox/index.js
@@ -1,8 +1,22 @@
 const continueBtn = document.getElementById("continue-btn");
+const acceptTerms=document.getElementById("accept-terms")
+const mailList=document.getElementById("mail-list")
 
 continueBtn.addEventListener("click", function () {
-  
+  if(acceptTerms.checked){
+    console.log("terms accepted")
+  }
+  else{
+    console.log("terms refused")
+  }
+   if(mailList.checked){
+    console.log("MailList accepted")
+  }
+  else{
+    console.log("MailList refused")
+  }
 });
+
 
 // create another label in html with text "add me to your mailing list"
 // create a new input with type checkbox connect the checkbox to label
