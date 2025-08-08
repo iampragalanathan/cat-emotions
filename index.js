9d9fb4dac5b1e3464b5f6c747a746ab543c12737
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-08-08 14:23:10 +0530

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { catsData } from "./data.js"
 
 const emotionsRadios=document.getElementById("emotion-radios")
-
+const getImageBtn=document.getElementById("get-image-btn")
 
 function getEmotionsArray(cats){
    const emotionsArray=[]
@@ -44,10 +44,22 @@ renderEmotionsRadios(catsData)
 emotionsRadios.addEventListener("change",highlightCheckedOption)
 
 function highlightCheckedOption(e){
+  const radios=document.getElementsByClassName("radio")
+  for (let radio of radios){
+    radio.classList.remove("highlight")
+  }
 
     document.getElementById(e.target.id).parentElement.classList.add("highlight")
 }
 
+getImageBtn.addEventListener("click",getMatchingCatsArray)
+
+function getMatchingCatsArray(){
+  if(document.querySelector(`input[type="radio"]:checked`)){
+
+    console.log(document.querySelector(`input[type]="radio":checked`).value)
+  }
+}
 
 
 
